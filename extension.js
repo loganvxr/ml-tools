1c11569456d4ee745461dc60c0261506524d201c
loganvxr <111173907+loganvxr@users.noreply.github.com>
2026-03-28 15:06:05 -0400

    Testing out a 'tanh' reporter block

--- extension.js
+++ extension.js
@@ -1,1 +1,35 @@
-// This is a placeholder file.
+(function(Scratch) {
+    'use strict';
+
+    class Extension {
+        getInfo() {
+            return {
+                id: "ml-tools",
+                name: "ML Tools",
+                color1: "#4A90D9",
+                color2: "#4A90D9",
+                color3: "#4381c5",
+                blocks: [
+                    [
+                        {
+                            opcode: 'tanh',
+                            text: 'tanh [X]',
+                            blockType: Scratch.BlockType.REPORTER,
+                            arguments: {
+                                X: {
+                                    type: Scratch.ArgumentType.NUMBER,
+                                    defaultValue: 0
+                                }
+                            }
+                        }
+                    ]   
+                ]
+            }
+        }
+
+        tanh({ X }) {
+            return Math.tanh(X);
+        }
+    }
+    Scratch.extensions.register(new Extension());
+}(Scratch))
